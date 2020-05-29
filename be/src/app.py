ce576debc6304e206b77c46caca44fd626a5563b
Michael Landry <mlandry8@outlook.com>
2020-05-29 03:01:43 -0400

    complete oath, clean up files

--- be/src/app.py
+++ be/src/app.py
@@ -1,11 +1,46 @@
-from flask import Flask, make_response
+import os
+import requests
+import base64
+
+from flask import Flask, make_response, request
 app = Flask(__name__)
+
+CLIENT_SECRET = os.environ.get('SECRET_KEY')
+CLIENT_ID = '69bb29ca8dfd409c8e267eaddff523cf'
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return make_response({'status':'success'}, 200)
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'OPTIONS', 'GET'])
 def login():
+    encoded_keys = base64.standard_b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode('utf-8')).decode()
 
-    return make_response({}, 200)
+    headers = {
+        'Authorization': f'Basic {encoded_keys}'
+    }
+
+    response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data={
+            'grant_type': 'authorization_code',
+            'code': request.json.get('code'),
+            'redirect_uri': request.json.get('redirectUri')
+        },
+        headers=headers
+    )
+
+    if response.status_code == 200:
+        return make_response({
+                'status':'success',
+                'accessToken': response.json().get('access_token')
+            },
+            200
+        )
+    else:
+        return make_response({
+                'status':'failure',
+                'error': response.json()
+            },
+            response.status_code
+        )
---
+++ fe/src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button } from 'primereact/button';
+
+const Home = ({params}) => {
+   const [accessToken, setAccessToken] = useState();
+
+   const code = params.get('code');
+   const redirectUri = 'http://localhost:3000/';
+
+   useEffect (()=>{
+      if (code) {
+         axios.post('http://localhost:3000/login', {
+            code: code,
+            redirectUri: redirectUri
+          })
+          .then(function (response) {
+            console.log(response.data.accessToken);
+            setAccessToken(response.data.accessToken)
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    }, [code])
+
+   useEffect (() => {
+      const authString = 'Bearer ' + accessToken
+      axios.get('https://api.spotify.com/v1/me', {headers: {Authorization: authString}}
+      ).then(function (response) {
+         console.log(response.data);
+       })
+       .catch(function (error) {
+         console.log(error);
+       });
+   }, [accessToken])
+
+   const handleLogin = () => {
+      const clientId = '69bb29ca8dfd409c8e267eaddff523cf'
+      window.location.replace(
+         'https://accounts.spotify.com/authorize?' +
+         'client_id=' + clientId + 
+         '&response_type=code' +
+         '&redirect_uri=' + redirectUri
+      );
+   }
+
+   return (
+      <Button label='Login w/ Spotify' onClick={handleLogin} />
+   );
+}
+
+export default Home
